2f04760a5d98dbfabb89d22c2b9d185277d6d715
yangbaojiang <yangbaojiang@hailiang.com>
2023-11-21 00:15:51 +0800

    树的是算法学习

--- src/main/java/labuladong/A1_Tree_最小深度_BFS_层序遍历法.java
+++ src/main/java/labuladong/A1_Tree_最小深度_BFS_层序遍历法.java
@@ -4,7 +4,9 @@
  */
 package labuladong;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import base.TreeNode;
@@ -70,6 +72,145 @@ public class A1_Tree_最小深度_BFS_层序遍历法 {
             depth++;
         }
         return depth;
+    }
+
+    /**
+     *     3
+     *    / \
+     *   9   20
+     *      / \
+     *     15 7
+     *      \
+     *      4
+     * @return
+     */
+    public static TreeNode createBinTree() {
+        TreeNode leaf7 = new TreeNode(7);
+        TreeNode leaf4 = new TreeNode(4);
+
+        TreeNode parent15 = new TreeNode(15, null, leaf4);
+
+        TreeNode left9 = new TreeNode(9, null, null);
+        TreeNode right20 = new TreeNode(20, parent15, leaf7);
+
+        return new TreeNode(3, left9, right20);
+    }
+
+    public static void main(String[] args) {
+        // 测试用例
+        TreeNode root = createBinTree();
+        // traverse(root);
+        // System.out.println(res);
+        // dfsWithLevelPrint(root);
+//        List<List<Integer>> lists = dfsWithLevel(root);
+//        System.out.println(lists);
+
+        dfsItePrint(root);
+
+    }
+
+    /**
+     * 打印每一层
+     * @param root
+     */
+    public static void dfsWithLevelPrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> levelQueue = new LinkedList<>();
+
+        queue.offer(root);
+        levelQueue.offer(0);
+
+        int curLevel = 0;
+        System.out.print("Level " + curLevel + ": ");
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            int level = levelQueue.poll();
+
+            if (level != curLevel) {
+                curLevel = level;
+                System.out.println();
+                System.out.print("Level " + curLevel + ": ");
+            }
+
+            System.out.print(node.val + " ");
+
+            if (node.left != null) {
+                queue.offer(node.left);
+                levelQueue.offer(level + 1);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+                levelQueue.offer(level + 1);
+            }
+        }
+    }
+
+    /**
+     * 记录每一层,迭代法
+     */
+    public static List<List<Integer>> dfsWithLevel(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        dfsRec(res, root, 0);
+        return res;
+    }
+
+    private static void dfsRec(List<List<Integer>> res, TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+
+        // 每层开始时新建一个列表
+        if (res.size() == level) {
+            res.add(new LinkedList<>());
+        }
+        res.get(level).add(root.val);
+        dfsRec(res, root.left, level + 1);
+        dfsRec(res, root.right, level + 1);
+    }
+
+    public static void dfsItePrint(TreeNode root) {
+        List<List<Integer>> lists = dfsIte(root);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+
+    }
+
+    /**
+     * 记录没一层,递归法
+     */
+    public static List<List<Integer>> dfsIte(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> levelList = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(levelList);
+        }
+        return res;
     }
 
 }
--- src/main/java/labuladong/A1_Tree_最小深度_DFS_递归法.java
+++ src/main/java/labuladong/A1_Tree_最小深度_DFS_递归法.java
@@ -4,13 +4,9 @@
  */
 package labuladong;
 
-import base.TreeNode;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.Stack;
+
+import base.TreeNode;
 
 /**
  * @author Baojiang Yang
@@ -21,7 +17,7 @@ public class A1_Tree_最小深度_DFS_递归法 {
     /**
      * DFS 框架: 迭代法
      */
-    public void dfs(TreeNode root) {
+    public static void dfs(TreeNode root) {
         if (root == null) {
             return;
         }
@@ -137,118 +133,8 @@ public class A1_Tree_最小深度_DFS_递归法 {
     public static void main(String[] args) {
         // 测试用例
         TreeNode root = createBinTree();
-        //  traverse(root);
-        //  System.out.println(res);
-         //dfsWithLevelPrint(root);
-//        List<List<Integer>> lists = dfsWithLevel(root);
-//        System.out.println(lists);
-
-        dfsLopPrint(root);
-
-    }
-
-    /**
-     * 打印每一层
-     * @param root
-     */
-    public static void dfsWithLevelPrint(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        Queue<Integer> levelQueue = new LinkedList<>();
-
-        queue.offer(root);
-        levelQueue.offer(0);
-
-        int curLevel = 0;
-        System.out.print("Level " + curLevel + ": ");
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            int level = levelQueue.poll();
-
-            if (level != curLevel) {
-                curLevel = level;
-                System.out.println();
-                System.out.print("Level " + curLevel + ": ");
-            }
-
-            System.out.print(node.val + " ");
-
-            if (node.left != null) {
-                queue.offer(node.left);
-                levelQueue.offer(level + 1);
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-                levelQueue.offer(level + 1);
-            }
-        }
-    }
-
-    /**
-     * 记录每一层,迭代法
-     */
-    public static List<List<Integer>>  dfsWithLevel(TreeNode root) {
-        List<List<Integer>> res = new LinkedList<>();
-        if (root == null) {
-            return res;
-        }
-        dfsRec(res, root, 0);
-        return res;
-    }
-
-    private static void dfsRec(List<List<Integer>> res, TreeNode root, int level) {
-        if (root == null) {
-            return;
-        }
-
-        // 每层开始时新建一个列表
-        if (res.size() == level) {
-            res.add(new LinkedList<>());
-        }
-        res.get(level).add(root.val);
-        dfsRec(res, root.left, level + 1);
-        dfsRec(res, root.right, level + 1);
-    }
-
-    public static void dfsLopPrint(TreeNode root){
-        List<List<Integer>> lists = dfsLop(root);
-        for (List<Integer> list : lists) {
-            System.out.println(list);
-        }
-
-    }
-
-    /**
-     * 记录没一层,递归法
-     */
-    public static List<List<Integer>> dfsLop(TreeNode root) {
-        List<List<Integer>> res = new LinkedList<>();
-        if (root == null) {
-            return res;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> levelList = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                levelList.add(node.val);
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-            res.add(levelList);
-        }
-        return res;
+        //traverse(root);
+        dfs(root);
     }
 
 }
