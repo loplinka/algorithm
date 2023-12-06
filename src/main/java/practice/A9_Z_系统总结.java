4d51601b7df22b748f7df3c6eaf41c10fccffe83
yangbaojiang <yangbaojiang@hailiang.com>
2023-12-06 08:55:44 +0800

    编码问题

---
+++ src/main/java/practice/A9_Z_系统总结.java
@@ -0,0 +1,24 @@
+/**
+ * Hailiang.com Inc.
+ * Copyright (c) 2004-2023 All Rights Reserved.
+ */
+package practice;
+
+/**
+ *
+ * @author Baojiang Yang
+ * @version : A9_Z_系统总结.java, v 0.1 2023年12月06日 08:55  Baojiang Yang Exp $
+ */
+public class A9_Z_系统总结 {
+    /**
+     *
+     * 一.实时规则引擎
+     *
+     *
+     *
+     * 二.产品工厂方案
+     *
+     *
+     *
+     */
+}
