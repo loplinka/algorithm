16f0ffdd99136a0a92132929700fedad043ce56d
yangbaojiang <yangbaojiang@hailiang.com>
2024-01-09 12:11:34 +0800

    编码问题

--- src/main/java/alg/A0_TEST.java
+++ src/main/java/alg/A0_TEST.java
@@ -15,12 +15,12 @@ public class A0_TEST {
         Integer b = 2;
         Integer c = 3;
         a = b;
-        System.out.println(a);
+        System.out.println("a=" + a);
         b = c;
-        System.out.println(a);
+        System.out.println("a=" +a);
         c = new Integer(4);
-        System.out.println(a);
-        System.out.println(c);
+        System.out.println("a=" +a);
+        System.out.println("c=" +c);
 
 
     }
